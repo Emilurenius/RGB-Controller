2e7586fe49c7db3e4e5e56d4c606db01b8c75785
Emil_Christiansen <emilsen68@gmail.com>
2023-08-08 00:05:46 +0200

    Use all the pixels

--- strip-python/rpi.py
+++ strip-python/rpi.py
@@ -3,7 +3,7 @@ from animator import Animator
 from rpi_ws281x import PixelStrip, Color
 
 # LED strip configuration:
-LED_COUNT = 16        # Number of LED pixels.
+LED_COUNT = 90        # Number of LED pixels.
 LED_PIN = 21          # GPIO pin connected to the pixels (18 uses PWM!).
 # LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
